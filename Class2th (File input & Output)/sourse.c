8982438a714fe3e2008d0e85be375b3e9d634d34
Kimdohyeong12 <hyeong030909@gmail.com>
2025-08-19 19:45:37 +0900

    (2025.8.19)좌표 설정 시스템

--- Class2th (File input & Output)/sourse.c
+++ Class2th (File input & Output)/sourse.c
@@ -1,10 +1,11 @@
 ﻿#include <stdio.h>
+#include <Windows.h>
 
 #define SIZE 10
 
 int main()
 {
-#pragma region GetAsynckeyState
+#pragma region GetAsyncKeyState
 	// Window API에서 제공되는 입력 처리 함수로, 지정된 키의 상태를
 	// 비동적으로 확인할 때 사용되는 함수입니다.
 
@@ -18,19 +19,39 @@ int main()
 
 	// 0x8001 : 이전에 누른 적이 있고, 호출 시점에도 눌려있는 상태
 
-	const char* dialog[SIZE];
-
-	dialog[0] = "안녕하세요...?";
-	dialog[1] = "누구신지...?";
-	dialog[2] = "탐정입니다";
-	dialog[3] = "아 네 탐정님 제때 오셨군요";
-	dialog[4] = "그래서... 무슨 의뢰를....";
-	dialog[5] = "저를 살해하려는 사람이 있는것 같아서요";
-	dialog[6] = "살해미수범 색출이죠?";
-	dialog[7] = "이해가 빠르시네요";
-	dialog[8] = "살해범 색출은 내 전문이죠";
-	dialog[9] = "그러시니 안심이 되네요";
-	
+	// const char* dialog[SIZE];
+	// 
+	// dialog[0] = "안녕하세요";
+	// dialog[1] = "누구신지...?";
+	// dialog[2] = "탐정입니다";
+	// dialog[3] = "아 네 탐정님 제때 오셨군요";
+	// dialog[4] = "그래서... 무슨 의뢰를....";
+	// dialog[5] = "저를 살해하려는 사람이 있는것 같아서요";
+	// dialog[6] = "살해미수범 색출이죠?";
+	// dialog[7] = "이해가 빠르시네요";
+	// dialog[8] = "살해범 색출은 내 전문이죠";
+	// dialog[9] = "그러시니 안심이 되네요";
+	// 
+	// int i = 0;
+	// 
+	// while (i < SIZE)
+	// {
+	// 	if (GetAsyncKeyState(VK_SPACE) & 0x0001)
+	// 	{
+	// 		if (i % 2 == 0)
+	// 		{
+	// 			printf("탐정 : %s\n", dialog[i]);
+	// 		}
+	// 		else
+	// 		{
+	// 			printf("의뢰인 : %s\n", dialog[i]);
+	// 		}
+	// 
+	// 		i++;
+	// 	}
+	// }
+	// 
+	// printf("대화가 종료되었습니다.\n");
 #pragma endregion
 
 
---
+++ Class3th (input System)/sourse.c
@@ -0,0 +1,47 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+
+#define UP 72
+#define LEFT 75
+#define RIGHT 77
+#define DOWN 80
+
+void move(int x, int y)
+{
+	// x와 y축을 설정하는 구조체입니다.
+	COORD position = { x, y };
+
+	// 콘솔 커서의 좌표를 설정하는 함수입니다.
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
+
+	int i = 0;
+
+	switch (i = UP)
+	{
+		printf("UP");
+	}
+
+	switch (i = LEFT)
+	{
+		printf("LEFT");
+	}
+
+	switch (i = RIGHT)
+	{
+		printf("RIGHT");
+	}
+
+	switch (i = DOWN)
+	{
+		printf("DOWN");
+	}
+}
+
+int main()
+{
+	move(5, 5);
+
+	printf("★");
+
+	return 0;
+}
